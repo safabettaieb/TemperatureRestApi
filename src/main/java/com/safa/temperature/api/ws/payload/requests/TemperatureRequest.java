8885fce3aaa457ce80133a6a3e9e8507fd1b6a39
Safa Bettaieb <safa.bettaieb1@gmail.com>
2020-02-25 21:41:52 +0100

    create Request and Response classes

---
+++ src/main/java/com/safa/temperature/api/ws/payload/requests/TemperatureRequest.java
@@ -0,0 +1,9 @@
+package com.safa.temperature.api.ws.payload.requests;
+
+import lombok.Data;
+
+@Data
+public class TemperatureRequest {
+	private int Temperature [] ;
+
+}
---
+++ src/main/java/com/safa/temperature/api/ws/payload/responses/TemperatureResponse.java
@@ -0,0 +1,9 @@
+package com.safa.temperature.api.ws.payload.responses;
+
+import lombok.Data;
+
+@Data
+public class TemperatureResponse {
+	private int resTemp ;
+
+}
