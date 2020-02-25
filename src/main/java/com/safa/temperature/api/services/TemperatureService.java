e5d203dca398fdf31f34536061cd47ac53cec293
Safa Bettaieb <safa.bettaieb1@gmail.com>
2020-02-25 23:53:24 +0100

    add closestToZero method in TemperatureService

--- src/main/java/com/safa/temperature/api/services/TemperatureService.java
+++ src/main/java/com/safa/temperature/api/services/TemperatureService.java
@@ -9,31 +9,58 @@ import com.safa.temperature.api.payload.responses.TemperatureResponse;
 
 @Service
 public class TemperatureService {
-	
-   public TemperatureResponse min(TemperatureRequest request) {
-	   
-	   TemperatureResponse response = new TemperatureResponse();
-	   int tab[] = request.getTemperatures(); 
-	    Arrays.sort(tab);
-	   response.setResTemp(tab[0]);
-	   return response;
-   
-   }
-   public TemperatureResponse max(TemperatureRequest request) {
-	   TemperatureResponse response = new TemperatureResponse();
-	   int tab[] = request.getTemperatures();
-	   Arrays.sort(tab);
-	   response.setResTemp(tab[tab.length-1]);
-	   return response;
-	   
-   }
-   public TemperatureResponse avg(TemperatureRequest request) {
-	   TemperatureResponse response = new TemperatureResponse();
-	   int tab[] = request.getTemperatures();
-	   int average = (int)Arrays.stream(tab).average().getAsDouble();
-	   response.setResTemp(average);
-	   return response;
-	  }
-   }
 
+	public TemperatureResponse min(TemperatureRequest request) {
 
+		TemperatureResponse response = new TemperatureResponse();
+		int tab[] = request.getTemperatures();
+		Arrays.sort(tab);
+		response.setResTemp(tab[0]);
+		return response;
+
+	}
+
+	public TemperatureResponse max(TemperatureRequest request) {
+		TemperatureResponse response = new TemperatureResponse();
+		int tab[] = request.getTemperatures();
+		Arrays.sort(tab);
+		response.setResTemp(tab[tab.length - 1]);
+		return response;
+
+	}
+
+	public TemperatureResponse avg(TemperatureRequest request) {
+		TemperatureResponse response = new TemperatureResponse();
+		int tab[] = request.getTemperatures();
+		int average = (int) Arrays.stream(tab).average().getAsDouble();
+		response.setResTemp(average);
+		return response;
+	}
+
+	public TemperatureResponse closetToZero(TemperatureRequest request) {
+		
+		TemperatureResponse response = new TemperatureResponse();
+		int tab[] = request.getTemperatures();
+		Arrays.sort(tab);
+
+		int pre=0, post=0, i = 0;
+		boolean verif = false;
+		
+		while (i < tab.length && verif) {
+			if (tab[i] == 0) {
+				pre = tab[i - 1];
+				post = tab[i + 1];
+			} else {
+				i++;
+			}
+		}
+		if (pre < 0) {
+			response.setResTemp(tab[post]);
+		} else {
+			response.setResTemp(tab[pre]);
+		}
+		return response;
+
+	}
+
+}
